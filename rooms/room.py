527c223463796b02387adcdf56c8a8c8c2c6ee53
Arikuvfx <127344248+Arikuvfx@users.noreply.github.com>
2026-01-10 23:04:13 +0100

    Update room.py

--- rooms/room.py
+++ rooms/room.py
@@ -1,1 +1,11 @@
-
+class Room:
+    """Represents a game room/map"""
+    def __init__(self, name, width=2400, height=1800, group="Default"):
+        self.name = name
+        self.width = width
+        self.height = height
+        self.group = group
+        self.enemies = []
+        self.npcs = []
+        self.objects = []
+        self.spawn_point = (width // 2, height // 2)
