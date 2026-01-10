619013ec46347f370e7eff0617e1405347a0fcb4
Arikuvfx <127344248+Arikuvfx@users.noreply.github.com>
2026-01-10 22:49:28 +0100

    Update camera.py

--- core/camera.py
+++ core/camera.py
@@ -1,1 +1,15 @@
+from config.settings import SCREEN_WIDTH, SCREEN_HEIGHT, WORLD_WIDTH, WORLD_HEIGHT
 
+class Camera:
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+        self.x = 0
+        self.y = 0
+    
+    def update(self, target):
+        self.x = target.x - SCREEN_WIDTH // 2
+        self.y = target.y - (SCREEN_HEIGHT - 100) // 2
+        
+        self.x = max(0, min(self.x, WORLD_WIDTH - SCREEN_WIDTH))
+        self.y = max(0, min(self.y, WORLD_HEIGHT - (SCREEN_HEIGHT - 100)))
